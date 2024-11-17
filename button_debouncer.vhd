38527c9de9c5da359a1dbc8e6070d2c81bc11112
KerCrafter <clemkerneur@gmail.com>
2024-11-17 18:08:56 +0100

    generic to manage number of debounce clk rising edges

--- button_debouncer.vhd
+++ button_debouncer.vhd
@@ -3,6 +3,10 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity button_debouncer is
+	generic(
+		debounce_clk_cnt : integer
+	);
+	
 	port (
 		clk : in std_logic;
 		btn_in : in std_logic;
@@ -11,7 +15,7 @@ entity button_debouncer is
 end entity;
 
 architecture beh of button_debouncer is
-	signal cnt : integer range 0 to 65536 := 0;
+	signal cnt : integer range 0 to debounce_clk_cnt := 0;
 begin
 
 	process(clk)
@@ -20,13 +24,13 @@ begin
 		if rising_edge(clk) then
 			if btn_in = '0' then
 				cnt <= 0;
-			elsif btn_in = '1' and cnt /= 65536 then
+			elsif btn_in = '1' and cnt /= debounce_clk_cnt then
 				cnt <= cnt + 1;
 			end if;
 		end if;
 	
 	end process;
 
-	btn_debounced <= '1' when cnt = 65536 else '0';
+	btn_debounced <= '1' when cnt = debounce_clk_cnt else '0';
 	
 end architecture;
