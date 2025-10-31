f1227b0afa9b37bf0ab6bd927efabda9cbd36e1d
KerCrafter <clemkerneur@gmail.com>
2025-10-31 22:07:33 +0100

    add Verilog support

---
+++ button_debouncer.v
@@ -0,0 +1,23 @@
+module button_debouncer #(
+    parameter DEBOUNCE_CLK_CNT = 65536
+)(
+    input  wire clk,
+    input  wire btn_in,
+    output reg  btn_debounced = 1'b0
+);
+
+    reg [$clog2(DEBOUNCE_CLK_CNT)-1:0] cnt = 0;
+
+    always @(posedge clk) begin
+        if (btn_in == 1'b0) begin
+            cnt <= 0;
+            btn_debounced <= 1'b0;
+        end else begin
+            if (cnt < DEBOUNCE_CLK_CNT - 1)
+                cnt <= cnt + 1;
+            else
+                btn_debounced <= 1'b1;
+        end
+    end
+
+endmodule
