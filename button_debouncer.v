9e59a23335a996ad4a3c1b3d295b7aacd514cbd4
KerCrafter <clemkerneur@gmail.com>
2025-11-02 14:10:35 +0100

    reset management

--- button_debouncer.v
+++ button_debouncer.v
@@ -2,6 +2,7 @@ module button_debouncer #(
     parameter DEBOUNCE_CLK_CNT = 65536
 )(
     input  wire clk,
+    input  wire reset,
     input  wire btn_in,
     output reg  btn_debounced = 1'b0
 );
@@ -9,14 +10,19 @@ module button_debouncer #(
     reg [$clog2(DEBOUNCE_CLK_CNT)-1:0] cnt = 0;
 
     always @(posedge clk) begin
-        if (btn_in == 1'b0) begin
+        if (reset) begin
             cnt <= 0;
             btn_debounced <= 1'b0;
         end else begin
-            if (cnt < DEBOUNCE_CLK_CNT - 1)
-                cnt <= cnt + 1;
-            else
-                btn_debounced <= 1'b1;
+            if (btn_in == 1'b0) begin
+                cnt <= 0;
+                btn_debounced <= 1'b0;
+            end else begin
+                if (cnt < DEBOUNCE_CLK_CNT - 1)
+                    cnt <= cnt + 1;
+                else
+                    btn_debounced <= 1'b1;
+            end
         end
     end
 
